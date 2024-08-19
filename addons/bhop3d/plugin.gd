350ff7192ba14c24a1455a510958edaaa6515981
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-08-19 15:04:43 +0100

    Update plugin.gd

--- addons/bhop3d/plugin.gd
+++ addons/bhop3d/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("BHop3D", "CharacterBody3D", preload("src/bhop3d.gd"), preload("src/icon.png"))
+	add_custom_type("BHop3D", "CharacterBody3D", preload("src/bhop3d.cs"), preload("src/icon.png"))
 
 func _exit_tree():
 	remove_custom_type("BHop3D")
